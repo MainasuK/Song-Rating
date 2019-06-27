1da9a7bfc5acca53d30d5dd345741126dc33000c
CMK <cirno.mainasuk@gmail.com>
2019-06-28 03:46:30 +0800

    fix: Apple Script not works when sandbox enable issue

--- Song Rating/ViewController.swift
+++ Song Rating/ViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import Cocoa
+import ScriptingBridge
 
 class ViewController: NSViewController {
+    
+    let iTunes: iTunesApplication? = SBApplication(bundleIdentifier: "com.apple.iTunes")!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
         // Do any additional setup after loading the view.
+        //print(iTunes?.isRunning)
+        //print(iTunes!.version)
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        
+        DispatchQueue.global().async {
+            let target =  NSAppleEventDescriptor(bundleIdentifier: "com.apple.iTunes")
+            let status = AEDeterminePermissionToAutomateTarget(target.aeDesc, typeWildCard, typeWildCard, true)
+            
+            DispatchQueue.main.async {
+                print(status)
+            }
+        }
+        
     }
 
     override var representedObject: Any? {
